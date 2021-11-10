fcd165a991564cd94849fef5b71ce6dd3d3c82ee
mrattez <mrattez@student.42nice.fr>
2021-11-10 13:43:18 +0100

    Fixed norme

--- srcs/hooks.c
+++ srcs/hooks.c
@@ -6,21 +6,14 @@
 /*   By: mrattez <mrattez@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 11:17:46 by mrattez           #+#    #+#             */
-/*   Updated: 2021/11/10 11:13:48 by mrattez          ###   ########.fr       */
+/*   Updated: 2021/11/10 13:27:43 by mrattez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int	handle_keys(int keycode, t_workspace *workspace)
+int	handle_keys_2(int keycode, t_workspace *workspace)
 {
-	// printf("Keycode: %d\n", keycode);
-	if (keycode == K_ESC)
-	{
-		mlx_destroy_window(workspace->mlx, workspace->window);
-		free_workspace(workspace);
-		exit(0);
-	}
 	if (keycode == K_SPC)
 	{
 		workspace->fractal->offset_x = 0;
@@ -31,45 +24,50 @@ int	handle_keys(int keycode, t_workspace *workspace)
 	if (keycode >= 123 && keycode <= 126)
 	{
 		if (keycode == K_LEFT)
-			workspace->fractal->offset_x -= 1;
+			workspace->fractal->offset_x -= 10;
 		if (keycode == K_RIGHT)
-			workspace->fractal->offset_x += 1;
+			workspace->fractal->offset_x += 10;
 		if (keycode == K_DOWN)
-			workspace->fractal->offset_y += 1;
+			workspace->fractal->offset_y += 10;
 		if (keycode == K_UP)
-			workspace->fractal->offset_y -= 1;
-		threaded_draw(workspace);
-	}
-	if (keycode == 82)
-		workspace->mouse = !workspace->mouse;
-	if (keycode == K_PGUP || keycode == K_PGDOWN)
-	{
-		if (keycode == K_PGUP)
-			zoom(workspace, 10);
-		if (keycode == K_PGDOWN)
-			zoom(workspace, 0.1);
+			workspace->fractal->offset_y -= 10;
 		threaded_draw(workspace);
 	}
 	return (0);
 }
 
+int	handle_keys(int keycode, t_workspace *workspace)
+{
+	if (keycode == K_ESC)
+	{
+		mlx_destroy_window(workspace->mlx, workspace->window);
+		free_workspace(workspace);
+		exit(0);
+	}
+	if (keycode == 82)
+		workspace->mouse = !workspace->mouse;
+	if (keycode == K_PGUP || keycode == K_PGDOWN)
+	{
+		zoom(workspace, 10.0 - 9.9 * (keycode == K_PGDOWN));
+		threaded_draw(workspace);
+	}
+	handle_keys_2(keycode, workspace);
+	return (0);
+}
+
 int	handle_mouse(int button, int x, int y, t_workspace *workspace)
 {
-	(void) x;
-	(void) y;
 	if (button == M_LB)
 	{
-		workspace->fractal->offset_x += map(x, workspace->width, workspace->width / -2, workspace->width / 2);
-		workspace->fractal->offset_y += map(y, workspace->height, workspace->height / -2, workspace->height / 2);
+		workspace->fractal->offset_x += map(x, workspace->width,
+				workspace->width / -2, workspace->width / 2);
+		workspace->fractal->offset_y += map(y, workspace->height,
+				workspace->height / -2, workspace->height / 2);
 		threaded_draw(workspace);
 	}
 	if (button >= 4)
 	{
-		if (button == M_SU)
-			zoom(workspace, 1.2);
-		if (button == M_SD)
-			zoom(workspace, 0.8);
-		// printf("Scale: %f\n", workspace->fractal->scale);
+		zoom(workspace, 0.8 + 0.4 * (button == M_SU));
 		threaded_draw(workspace);
 	}
 	return (0);
@@ -80,8 +78,8 @@ int	mouse_move(int x, int y, t_workspace *workspace)
 	if (!workspace->mouse)
 		return (0);
 	workspace->mmx = map(x + workspace->fractal->offset_x, workspace->width,
-					workspace->width / (PPU * -2) / workspace->fractal->scale,
-					workspace->width / (PPU * 2) / workspace->fractal->scale);
+			workspace->width / (PPU * -2) / workspace->fractal->scale,
+			workspace->width / (PPU * 2) / workspace->fractal->scale);
 	workspace->mmy = map(y + workspace->fractal->offset_y, workspace->height,
 			workspace->height / (PPU * -2) / workspace->fractal->scale,
 			workspace->height / (PPU * 2) / workspace->fractal->scale);
@@ -91,7 +89,7 @@ int	mouse_move(int x, int y, t_workspace *workspace)
 
 void	init_hooks(t_workspace *workspace)
 {
-	mlx_key_hook(workspace->window, &handle_keys, workspace);
-	mlx_mouse_hook(workspace->window, &handle_mouse, workspace);
-	mlx_hook(workspace->window, 6, 0, &mouse_move, workspace);
+	mlx_key_hook(workspace->window, handle_keys, workspace);
+	mlx_mouse_hook(workspace->window, handle_mouse, workspace);
+	mlx_hook(workspace->window, 6, 0, mouse_move, workspace);
 }
